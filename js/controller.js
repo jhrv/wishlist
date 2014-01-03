da60a8557a82472ff2eb354fc7e0d574175ec90f
jhrv <johnny.horvi@gmail.com>
2014-01-03 20:45:36 +0100

    nå med link

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,11 @@
 var wishlistApp = angular.module('wishlistApp', []);
 
 wishlistApp.controller('wishlistCtrl', function ($scope){
+    $scope.applicationName = "Ønskeliste"
+
     $scope.wishes = [
-        {'title': 'turshorts'},
-        {'title': 'dagstursekk'},
-        {'title': 'sommerdekk'},
+        {'title': 'turshorts', 'link': 'https://www.norrona.com/en-GB/Products/7021-10/8851/fjora-shorts-m/'},
+        {'title': 'dagstursekk', 'link': ''},
+        {'title': 'sommerdekk', 'link': ''},
     ]
 });
